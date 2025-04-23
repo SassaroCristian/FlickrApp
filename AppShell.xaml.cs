1c894d20f72ec169477dcc741d81c245037d3612
SassaroCristian <sassaro01cristian@gmail.com>
2025-04-23 09:38:11 +0200

    commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlickrApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FlickrApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
