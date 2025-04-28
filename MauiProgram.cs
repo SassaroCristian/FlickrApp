02e37c3b9f2baef1eb76e0709299160038e4f9a4
SassaroCristian <sassaro01cristian@gmail.com>
2025-04-28 11:12:43 +0200

    added custom styles

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -7,10 +7,9 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
+        builder.UseMauiApp<App>()
             .ConfigureFonts(fonts =>
-            {
+            { 
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
