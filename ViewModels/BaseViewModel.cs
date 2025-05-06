19caa625d4b52ec756d0427634989c8be11add6d
andrea.kusuanco <nenhe34@gmail.com>
2025-05-06 16:54:38 +0200

    BaseViewModel.cs completed

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FlickrApp.ViewModels;
+
+public abstract partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty] private bool _isBusy;
+    public bool IsNotBusy => !IsBusy;
+
+    protected void ExecuteSafely(Action operation, Action<Exception>? onError = null, Action? onComplete = null)
+    {
+        if (IsBusy) return;
+
+        IsBusy = true;
+        try
+        {
+            operation.Invoke();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error during execution: {ex.Message}");
+            onError?.Invoke(ex);
+        }
+        finally
+        {
+            IsBusy = false;
+            onComplete?.Invoke();
+        }
+    }
+
+    protected async Task ExecuteSafelyAsync(Func<Task> operation, Action<Exception>? onError = null,
+        Action? onComplete = null)
+    {
+        if (IsBusy) return;
+
+        IsBusy = true;
+        try
+        {
+            ArgumentNullException.ThrowIfNull(operation);
+            await operation();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error during execution: {ex.Message}");
+            onError?.Invoke(ex);
+        }
+        finally
+        {
+            IsBusy = false;
+            onComplete?.Invoke();
+        }
+    }
+
+    protected async Task<TResult?> ExecuteSafelyAsync<TResult>(Func<Task<TResult>> operation,
+        Action<Exception>? onError = null, Action? onComplete = null)
+    {
+        if (IsBusy) return default;
+
+        IsBusy = true;
+        TResult? result = default;
+        try
+        {
+            ArgumentNullException.ThrowIfNull(operation);
+            result = await operation();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error during execution: {ex.Message}");
+            onError?.Invoke(ex);
+        }
+        finally
+        {
+            IsBusy = false;
+            onComplete?.Invoke();
+        }
+
+        return result;
+    }
+}
