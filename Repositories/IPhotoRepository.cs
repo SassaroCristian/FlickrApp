9d575766f6e3918f1de0baed2b4198ffb8cdda74
SassaroCristian <sassaro01cristian@gmail.com>
2025-05-06 11:25:38 +0200

    creation of IPhotoRepository interface

--- Repositories/IPhotoRepository.cs
+++ Repositories/IPhotoRepository.cs
@@ -1,9 +1,15 @@
 using FlickrApp.Entities;
-
+using System.Threading.Tasks;
 namespace FlickrApp.Repositories;
 
 public interface IPhotoRepository
 {
     public List<Photo> GetPhotos();
-    Task<Photo> GetPhotoByIdAsync
+    Task<List<Photo>> GetAllPhotosAsync(); 
+    Task<Photo> GetPhotoByIdAsync(string id); 
+    Task<int> SavePhotoAsync(Photo photo); 
+    Task<int> DeletePhotoAsync(string id); 
+    Task<List<Photo>> SearchPhotosByTitleAsync(string title);
+    Task<List<Photo>> GetPhotosWithLocalFilesAsync(); 
+    Task<bool> IsPhotoSavedLocallyAsync(string photoId); 
 }
