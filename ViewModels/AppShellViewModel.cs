688e9b612a03e4408f6bfae42642f90546af2524
andrea.kusuanco <nenhe34@gmail.com>
2025-05-07 15:07:48 +0200

    implemented BaseViewModel.cs into AppShellViewModel.cs

--- ViewModels/AppShellViewModel.cs
+++ ViewModels/AppShellViewModel.cs
@@ -1,10 +1,11 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using FlickrApp.Models;
 using FlickrApp.Services;
+using FlickrApp.ViewModels.Base;
 
 namespace FlickrApp.ViewModels;
 
-public partial class AppShellViewModel : ObservableObject
+public partial class AppShellViewModel : BaseViewModel
 {
     private readonly IFlickrApiService? _flickr;
     [ObservableProperty] private string _headerBackgroundSource = "flyout_header.jpg";
@@ -17,27 +18,30 @@ public partial class AppShellViewModel : ObservableObject
     public AppShellViewModel(IFlickrApiService flickr)
     {
         _flickr = flickr;
-        Task.Run(GetBackgroundSourceAsync);
+        _ = GetBackgroundSourceAsync();
     }
 
     partial void OnIsFlyoutOpenChanged(bool value)
     {
         if (value) return;
-        Task.Run(GetBackgroundSourceAsync);
+        _ = GetBackgroundSourceAsync();
     }
 
     private async Task GetBackgroundSourceAsync()
     {
-        if (_flickr == null) return;
+        await ExecuteSafelyAsync(async () =>
+        {
+            if (_flickr == null) return;
 
-        if (_photos.Count == 0)
-            _photos = await _flickr.SearchAsync("background", string.Empty);
+            if (_photos.Count == 0)
+                _photos = await _flickr.SearchAsync("background", string.Empty);
 
-        var index = Random.Shared.Next(0, _photos.Count);
-        while (_lastIndex == index)
-            index = Random.Shared.Next(0, _photos.Count);
+            var index = Random.Shared.Next(0, _photos.Count);
+            while (_lastIndex == index)
+                index = Random.Shared.Next(0, _photos.Count);
 
-        HeaderBackgroundSource = _photos[index].MediumUrl;
-        _lastIndex = index;
+            HeaderBackgroundSource = _photos[index].MediumUrl;
+            _lastIndex = index;
+        });
     }
 }
