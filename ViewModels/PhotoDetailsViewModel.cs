6d7074002f44d4255ea3a6939f639d65b102e121
andrea.kusuanco <nenhe34@gmail.com>
2025-05-07 15:35:12 +0200

    integrated BaseViewModel.cs into PhotoDetailsViewModel.cs

--- ViewModels/PhotoDetailsViewModel.cs
+++ ViewModels/PhotoDetailsViewModel.cs
@@ -2,12 +2,13 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using FlickrApp.Models;
 using FlickrApp.Services;
+using FlickrApp.ViewModels.Base;
 using Debug = System.Diagnostics.Debug;
 
 namespace FlickrApp.ViewModels;
 
 [QueryProperty(nameof(PhotoId), nameof(PhotoId))]
-public partial class PhotoDetailsViewModel(IFlickrApiService flickr) : Base.BaseViewModel
+public partial class PhotoDetailsViewModel(IFlickrApiService flickr) : BaseViewModel
 {
     [ObservableProperty] [NotifyPropertyChangedFor(nameof(CommentsHeaderTitle))]
     private ObservableCollection<FlickrComment> _comments = [];
@@ -20,7 +21,7 @@ public partial class PhotoDetailsViewModel(IFlickrApiService flickr) : Base.Base
     partial void OnPhotoIdChanged(string value)
     {
         Debug.WriteLine("photoId changed: " + value + "");
-        Task.Run(FillData);
+        _ = FillData();
     }
 
     private async Task FillData()
@@ -31,6 +32,7 @@ public partial class PhotoDetailsViewModel(IFlickrApiService flickr) : Base.Base
         // GETTING DETAILS
         await ExecuteSafelyAsync(async () =>
         {
+            Debug.WriteLine(" ---> Getting details ...");
             var details = await flickr.GetDetailsAsync(PhotoId);
             Details = details;
         });
@@ -38,6 +40,7 @@ public partial class PhotoDetailsViewModel(IFlickrApiService flickr) : Base.Base
         // GETTING COMMENTS
         await ExecuteSafelyAsync(async () =>
         {
+            Debug.WriteLine(" --- Getting comments ...");
             var comments = await flickr.GetCommentsAsync(PhotoId);
             Comments = new ObservableCollection<FlickrComment>(comments);
         });
