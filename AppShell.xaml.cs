39ec236e52d4ef2bc928ced64c0fc74ad2a7c5c1
Andrea Kusuanco <andrea.kusuanco.dev@gmail.com>
2025-05-15 15:27:15 +0200

    push check

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -8,7 +8,6 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-        
         Routing.RegisterRoute(nameof(DiscoverPage), typeof(DiscoverPage));
         Routing.RegisterRoute(nameof(PhotoDetailsPage), typeof(PhotoDetailsPage));
         Routing.RegisterRoute(nameof(MapsPage), typeof(MapsPage));
